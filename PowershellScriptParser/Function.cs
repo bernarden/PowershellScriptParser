c0f46594edbb1e79f0bb10710f95f6d930126f96
Victor Usoltsev <bernarden@mail.ru>
2019-02-14 01:11:54 +1300

    Added code.

---
+++ PowershellScriptParser/Function.cs
@@ -0,0 +1,9 @@
+﻿namespace PowershellScriptParser
+{
+    public class Function
+    {
+        public string Name { get; set; }
+        public long StartLine { get; set; }
+        public long EndLine { get; set; }
+    }
+}
---
+++ PowershellScriptParser/FunctionExecution.cs
@@ -0,0 +1,9 @@
+﻿namespace PowershellScriptParser
+{
+    public class FunctionExecution
+    {
+        public long ExecutionLine { get; set; }
+        public string FunctionName { get; set; }
+        public string ExecutorName { get; set; }
+    }
+}
